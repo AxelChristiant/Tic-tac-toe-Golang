fc6abbbcdd813ec753265449c2ac8e9381348914
axelchristiant <axel.kusuma@redawning.com>
2022-08-31 00:20:29 +0700

    Finished

--- main.go
+++ main.go
@@ -13,8 +13,11 @@ var ThreeConsecutiveO = [3]string{
 	"O", "O", "O"}
 var ThreeConsecutiveX = [3]string{
 	"X", "X", "X"}
+var playerTurn = "player 1"
+var playerMark = map[string]string{"player 1": "O", "player 2": "X"}
 
 func displayBoard() {
+	fmt.Println()
 	for i := 1; i <= 9; i++ {
 		if i%3 == 0 {
 			fmt.Printf("%s\n", board[i-1])
@@ -22,6 +25,7 @@ func displayBoard() {
 			fmt.Printf("%s|", board[i-1])
 		}
 	}
+	fmt.Println()
 }
 
 func isConsecutiveColumn() bool {
@@ -56,10 +60,54 @@ func isConsecutiveDiagonal() bool {
 	return false
 }
 
-func checkingBoard() bool {
-	return isConsecutiveRow() || isConsecutiveColumn() || isConsecutiveDiagonal()
+func thereIsAWinner() bool {
+	if isConsecutiveRow() || isConsecutiveColumn() || isConsecutiveDiagonal() {
+		fmt.Printf("%s Win!\n", playerTurn)
+		return true
+	} else {
+		return false
+	}
+}
+func thereIsEmptyField() bool {
+	for _, e := range board {
+		if e == "-" {
+			return true
+		}
+	}
+	fmt.Println("Draw")
+	return false
+}
+func playerMove() {
+	var idx int
+	fmt.Println("Choose the empty field with the number 1-9 : ")
+	fmt.Scanln(&idx)
+	idx -= 1
+	if idx < 0 || idx > 9 {
+		fmt.Println("Your input is not valid!")
+		playerMove()
+	} else if board[idx] != "-" {
+		fmt.Println("Field is already filled!")
+	} else {
+		board[idx] = playerMark[playerTurn]
+	}
+
 }
 
+func gameOn() bool {
+	return !thereIsAWinner() && thereIsEmptyField()
+}
+func changeTurn() {
+	if playerTurn == "player 1" {
+		playerTurn = "player 2"
+	} else {
+		playerTurn = "player 1"
+	}
+}
 func main() {
-	fmt.Println(checkingBoard())
+	for gameOn() {
+		displayBoard()
+		playerMove()
+		changeTurn()
+	}
+	displayBoard()
 }
